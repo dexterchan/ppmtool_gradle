e3087402f633ecc6154f51403e6f04d66daf5e19
dexterchan <dexterchan@gmail.com>
2019-03-28 22:52:47 +0800

    branch12

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <h1>Welcome to DashBoard</h1>;
+  }
+}
+
+export default Dashboard;
