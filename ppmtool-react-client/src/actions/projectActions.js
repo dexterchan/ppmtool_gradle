b857c54bebcd6277f94344a8fe5c460ad0d1b192
dexterchan <dexterchan@gmail.com>
2019-04-13 19:29:10 +0800

    add validation style

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { GET_ERRORS } from "./types";
 
+//curried functions here!
 export const createProject = (project, history) => async dispatch2 => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
