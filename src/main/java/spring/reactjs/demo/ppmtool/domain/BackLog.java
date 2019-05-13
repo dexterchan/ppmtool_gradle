f18301bcebbcd7813cd83e69664ce7cdb1954d2e
dexterchan <dexterchan@gmail.com>
2019-05-13 18:54:34 +0800

    add group, repository

---
+++ src/main/java/spring/reactjs/demo/ppmtool/domain/BackLog.java
@@ -0,0 +1,54 @@
+package spring.reactjs.demo.ppmtool.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class BackLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer PTSequence=0;
+    private String projectIdentifier;
+
+    //OneToOne with project
+
+    //OneToMany with Project tasks
+
+    public BackLog(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getPTSequence() {
+        return PTSequence;
+    }
+
+    public void setPTSequence(Integer PTSequence) {
+        this.PTSequence = PTSequence;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    @Override
+    public String toString() {
+        return "BackLog{" +
+                "id=" + id +
+                ", PTSequence=" + PTSequence +
+                ", projectIdentifier='" + projectIdentifier + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/spring/reactjs/demo/ppmtool/repositories/BackLogRepository.java
@@ -0,0 +1,9 @@
+package spring.reactjs.demo.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import spring.reactjs.demo.ppmtool.domain.BackLog;
+
+@Repository
+public interface BackLogRepository extends CrudRepository<BackLog,Long> {
+}
