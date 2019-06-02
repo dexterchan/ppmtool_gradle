7ec1b3d25568fe1ef72385ca90dfdbfb8fc8b4b2
dexterchan <dexterchan@gmail.com>
2019-06-02 13:57:14 +0800

    checkin 38 task retrieval

--- src/main/java/spring/reactjs/demo/ppmtool/web/BackLogController.java
+++ src/main/java/spring/reactjs/demo/ppmtool/web/BackLogController.java
@@ -40,4 +40,12 @@ public class BackLogController {
         return (  projectTaskService.findBackLogById(backlog_id));
     }
 
+    @GetMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id){
+        ProjectTask projectTask = projectTaskService.findPTByProjectSequence(backlog_id,pt_id);
+
+        return new ResponseEntity<ProjectTask>( projectTask, HttpStatus.OK);
+    }
+
+
 }
